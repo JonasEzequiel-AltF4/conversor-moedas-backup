8609443dd8a7129aeded9aa3b529d1f3af27a616
Jonas Ezequiel <jonas.ezequiel@aluno.uepb.edu.br>
2026-07-14 21:46:40 +0000

    ✨ JavaScrip do Real para Euro
    Foi adicionado o JavaScript da parte do Real para euro.

--- CoApp.js
+++ CoApp.js
@@ -10,3 +10,20 @@ document.getElementById('convertBtn').addEventListener('click', function() {
     const converted = brlValue / usdRate;
     document.getElementById('result').innerText = `Resultado: $ ${converted.toFixed(2)} USD`;
 });
+document.getElementById('convertEuroBtn').addEventListener('click', function () {
+
+    const brlValue = parseFloat(document.getElementById('eurInput').value);
+    const euroRate = 6.00;
+
+    if (isNaN(brlValue) || brlValue <= 0) {
+        document.getElementById('resultEuro').innerText =
+            "Erro: Digite um valor maior que zero.";
+        return;
+    }
+
+    const converted = brlValue / euroRate;
+
+    document.getElementById('resultEuro').innerText =
+        `Resultado: € ${converted.toFixed(2)} EUR`;
+
+});
